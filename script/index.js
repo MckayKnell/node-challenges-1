3e79a6b4fd03dbd8ccd50378a5ec3f1ed226fa69
McKay Knell <mckay@devpipeline.com>
2024-03-04 01:09:49 -0700

    first commit

---
+++ script/index.js
@@ -0,0 +1,18 @@
+//challenge 1
+const darkKnight = document.getElementById("movie1");
+const inTime = document.getElementById("movie4");
+
+console.dir(darkKnight);
+inTime.style.color = "green";
+darkKnight.style["background-color"] = "red";
+
+//challenge 2
+const googleSearch = document.querySelector(".link");
+
+console.dir(googleSearch);
+
+//challenge 3
+
+document.querySelector(".column1").innerText = "red";
+document.querySelector(".column2").innerText = "black";
+document.querySelector(".column3").innerText = "white";
